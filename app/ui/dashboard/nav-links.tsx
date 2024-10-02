04a9f9788fae22e615e0c389042df910443c3efb
Yirun Zhou <yirunzhou@brandeis.edu>
2024-10-02 01:21:11 -0700

    Pathname

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
     UserGroupIcon,
     HomeIcon,
@@ -20,6 +22,8 @@ const links = [
 ];
 
 export default function NavLinks() {
+    const pathname = usePathname();
+
     return (
         <>
             {links.map((link) => {
@@ -28,7 +32,12 @@ export default function NavLinks() {
                     <Link
                         key={link.name}
                         href={link.href}
-                        className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+                        className={clsx(
+                            'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+                            {
+                                'bg-sky-100 text-blue-600': pathname === link.href,
+                            },
+                        )}
                     >
                         <LinkIcon className="w-6" />
                         <p className="hidden md:block">{link.name}</p>
